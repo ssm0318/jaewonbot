5688c8a49084035a677c7be6de5d36ede6c368c9
ssm0318 <kleenex0318@gmail.com>
2018-11-12 18:10:41 +0900

    test urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/keyboard' => 'basic#keyboard_init'
   post '/message' => 'basic#chat_control'
+  post '/friend/:user_key' => 'basic#new_friend'
+  delete '/friend/:user_key' => 'basic#deleted_friend'
+  delete '/chat_room/:user_key' => 'basic#left_chat'
 end
